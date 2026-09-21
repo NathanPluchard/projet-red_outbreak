d8caba9c57837a1d81848b3c31912d332ff92b25
LeoRo22 <leopaola.robin@gmail.com>
2026-09-21 12:16:22 +0200

    potion

--- src/potion/vie.go
+++ src/potion/vie.go
@@ -13,10 +13,9 @@ func NouvellePotionDeVie(nom string, soin int) Potion {
 }
 
 var PotionsDeVieDisponibles = []Potion{
-    NouvellePotionDeVie("Kit de chirurgie de fortune", 100),
-    NouvellePotionDeVie("Poche de perfusion", 75),
-    NouvellePotionDeVie("Bandage militaire", 50),
-    NouvellePotionDeVie("Pansement compressif", 25),
-    NouvellePotionDeVie("Seringue d'adrénaline", 15),
-}
-
+    NouvellePotionDeVie("Kit de chirurgie de fortune", 100, "Réparation quasi-totale des blessures, agit instantanément."),
+    NouvellePotionDeVie("Poche de perfusion", 75, "Stabilise et régénère progressivement une grande partie des PV."),
+    NouvellePotionDeVie("Bandage militaire", 50, "Soigne une blessure moyenne et réduit les saignements."),
+    NouvellePotionDeVie("Pansement compressif", 25, "Arrête rapidement une hémorragie légère et restaure un peu de vie."),
+    NouvellePotionDeVie("Seringue d'adrénaline", 15, "Redonne un peu d'énergie et quelques PV, effet immédiat mais faible."),
+} 
