8dfa4ec8cc7505f66e915e4afd1150385bc91a14
LeoRo22 <leopaola.robin@gmail.com>
2026-09-21 10:55:07 +0200

    poison

--- src/potion/poison.go
+++ src/potion/poison.go
@@ -12,9 +12,9 @@ func NouvellePotionDePoison(nom string, degats int) Potion {
 }
 
 var PotionsDePoisonDisponibles = []Potion{
-	NouvellePotionDePoison("Poison à explosion différée", 90),
-	NouvellePotionDePoison("Poison à stacks", 75),
-	NouvellePotionDePoison("Poison paralysant", 50),
-	NouvellePotionDePoison("Poison affaiblissant", 35),
-	NouvellePotionDePoison("Poison à dégâts sur la durée", 10),
+	NouvellePotionDePoison("Poison à explosion différée", 90, "Explose après quelques tours et inflige énormément de dégâts."),
+	NouvellePotionDePoison("Poison à stacks", 75,"Chaque application ajoute un stack et augmente les dégâts."),
+	NouvellePotionDePoison("Poison paralysant", 50, "Inflige des dégâts et peut empêcher la cible d'agir pendant un tour."),
+	NouvellePotionDePoison("Poison affaiblissant", 35, "Inflige des dégâts et réduit une statistique de la cible."),
+	NouvellePotionDePoison("Poison à dégâts sur la durée", 10, "Inflige simplement des dégâts pendant plusieurs tours."),
 }
